509ae233cdf5c16c35761afb8a8e84d1610bb3b6
canhhuy116 <canhhuy1162002@gmail.com>
2023-04-01 16:26:42 +0700

    middleware apiKey and permission

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,48 @@
+const { findById } = require('../services/apiKey.service');
+
+const HEADER = {
+  API_KEY: 'x-api-key',
+  AUTHORIZATION: 'Authorization',
+};
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString();
+    if (!key) {
+      return res.status(403).json({
+        message: 'Forbidden Error',
+      });
+    }
+    // check objKey
+    const objKey = await findById(key);
+    if (!objKey) {
+      return res.status(403).json({
+        message: 'Forbidden Error',
+      });
+    }
+    req.apiKey = objKey;
+    return next();
+  } catch (error) {}
+};
+
+const permission = (permission) => {
+  return (req, res, next) => {
+    const { apiKey } = req;
+    if (!apiKey) {
+      return res.status(403).json({
+        message: 'permission denied',
+      });
+    }
+
+    const validPermission = apiKey.permissions.includes(permission);
+    if (!validPermission) {
+      return res.status(403).json({
+        message: 'permission denied',
+      });
+    }
+
+    return next();
+  };
+};
+
+module.exports = { apiKey, permission };
